6d0a929a9cbd1d2233697293438f7ffedc58ff22
Thor Dueholm <s194589@student.dtu.dk>
2020-06-13 17:33:44 +0200

    Excersize chooser now autocomplete

--- app/src/main/java/com/example/youfit/ExcersizeFragment.java
+++ app/src/main/java/com/example/youfit/ExcersizeFragment.java
@@ -11,14 +11,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.android.gms.common.util.ArrayUtils;
 
 public class ExcersizeFragment extends Fragment {
 
     private String workoutSelected;
     private String excersizeSelected;
+    protected String[] excersizes;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -38,18 +43,25 @@ public class ExcersizeFragment extends Fragment {
         TextView excersizeSelectedTextView = view.findViewById(R.id.excersizeSelectedTextView);
         excersizeSelectedTextView.setText(workoutSelected);
 
+        this.excersizes = ArrayUtils.concat(getResources().getStringArray(R.array.rep_excersizes),
+                getResources().getStringArray(R.array.time_excersizes));
+
         Button addExcersizeBtn = view.findViewById(R.id.addExerciseBtn);
+
         addExcersizeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
                 builder.setTitle("Add excersize");
-                // I'm using fragment here so I'm using getView() to provide ViewGroup
-                // but you can provide here any other instance of ViewGroup from your Fragment / Activity
+                // inflate view
                 View viewInflated = LayoutInflater.from(getContext()).inflate(R.layout.popup_input_excersize, (ViewGroup) getView(), false);
                 // Set up the input
-                final EditText input = (EditText) viewInflated.findViewById(R.id.input);
-                // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                final AutoCompleteTextView inputExcersize = viewInflated.findViewById(R.id.inputExcersize);
+
+                ArrayAdapter<String> autocompleteAdapter = new ArrayAdapter<String>(getContext(),R.layout.support_simple_spinner_dropdown_item,excersizes);
+                inputExcersize.setAdapter(autocompleteAdapter);
+
+
                 builder.setView(viewInflated);
 
                 // Set up the buttons
@@ -57,7 +69,7 @@ public class ExcersizeFragment extends Fragment {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
-                        excersizeSelected = input.getText().toString();
+                        excersizeSelected = inputExcersize.getText().toString();
                     }
                 });
                 builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
