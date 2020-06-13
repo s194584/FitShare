9e0ed4a0aef93cefb8e2c3f30ea541216eb9e748
MaltheOrberg <s194584@student.dtu.dk>
2020-06-13 13:52:54 +0200

    Added BindingUI for LoginActivity

--- app/src/main/java/com/example/youfit/LoginActivity.java
+++ app/src/main/java/com/example/youfit/LoginActivity.java
@@ -1,18 +1,25 @@
 package com.example.youfit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import androidx.databinding.DataBindingUtil;
+
+import com.example.youfit.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends Activity {
+
+    ActivityLoginBinding mBinding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        mBinding = DataBindingUtil.setContentView(this ,R.layout.activity_login);
 
+        mBinding.setSignInMode(true);
 
         // Sign in
         findViewById(R.id.buttonSignin).setOnClickListener(new View.OnClickListener() {
@@ -27,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
         findViewById(R.id.textViewCreateNewProfile).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                mBinding.setSignInMode(!mBinding.getSignInMode());
             }
         });
 
