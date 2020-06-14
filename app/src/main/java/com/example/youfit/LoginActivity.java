e0e48928f7baacfdb9ac9fca1c3c228e5d097a46
Thor Dueholm <s194589@student.dtu.dk>
2020-06-14 15:24:34 +0200

    Made Signup possible and added temporary logout btn in settings

--- app/src/main/java/com/example/youfit/LoginActivity.java
+++ app/src/main/java/com/example/youfit/LoginActivity.java
@@ -4,41 +4,117 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 
 import com.example.youfit.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends Activity {
 
     ActivityLoginBinding mBinding;
 
+    protected EditText editTextEmailAddress, editTextPassword,editTextPasswordConfirm;
+    protected Button buttonSignin;
+    protected ProgressBar progressBarSignIn;
+    protected TextView textViewCreateNewProfile;
+
+    protected FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = DataBindingUtil.setContentView(this ,R.layout.activity_login);
-
         mBinding.setSignInMode(true);
 
-        // Sign in
-        findViewById(R.id.buttonSignin).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                startActivity(intent);
-                finish();
+        //load components
+        this.editTextEmailAddress = findViewById(R.id.editTextEmailAddress);
+        this.editTextPassword = findViewById(R.id.editTextPassword);
+        this.editTextPasswordConfirm = findViewById(R.id.editTextPasswordConfirm);
+        this.buttonSignin = findViewById(R.id.buttonSignin);
+        this.textViewCreateNewProfile = findViewById(R.id.textViewCreateNewProfile);
+        this.progressBarSignIn = findViewById(R.id.progressBarSignIn);
+
+        this.firebaseAuth = FirebaseAuth.getInstance(); //get current instance of database.
+
+        if(firebaseAuth.getCurrentUser() != null) {
+            goToMainActivity();
+        }
+
+
+    }
+
+    private void goToMainActivity() {
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    //click event for signin/signup button
+    public void onSignIn(View view) {
+        if(this.mBinding.getSignInMode()) { //sign in event
+            goToMainActivity();
+        } else { //sign up event
+
+            //Check validity of input, and give corresponding error message.
+            if (checkIfEmpty(this.editTextEmailAddress,"Email") &&
+                    checkIfEmpty(this.editTextPassword,"Password") &&
+                    checkIfEmpty(this.editTextPasswordConfirm,"Password")) {
+                return;
             }
-        });
 
-        findViewById(R.id.textViewCreateNewProfile).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                mBinding.setSignInMode(!mBinding.getSignInMode());
+            //load strings
+            String mEmail = this.editTextEmailAddress.getText().toString().trim();
+            String mPassword = this.editTextPassword.getText().toString().trim();
+            String mConfirmPassword = this.editTextPasswordConfirm.getText().toString().trim();
+
+            //More validity checks
+            if (!mPassword.equals(mConfirmPassword)) {
+                this.editTextPasswordConfirm.setError("Passwords must match");
             }
-        });
 
-        findViewById(R.id.textViewCreateNewProfile);
 
+            this.progressBarSignIn.setVisibility(View.VISIBLE);
+
+
+            //registration of user
+            this.firebaseAuth.createUserWithEmailAndPassword(mEmail,mPassword).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "", Toast.LENGTH_SHORT);
+                    if (task.isSuccessful()) {
+                        toast.setText("Successfully created user!");
+                        goToMainActivity();
+                    } else {
+                        toast.setText("Error: " + task.getException().getMessage());
+                    }
+                    toast.show();
+                }
+            });
+
+
+        }
+    }
+
+    private boolean checkIfEmpty(EditText editText, String requriedMessage) {
+        if (editText.getText().toString().trim().isEmpty()) {
+            editText.setError(requriedMessage + " is required");
+            return true;
+        }
+        return false;
+    }
+
+    //click event for create new profile/back textview
+    public void onCreateNewProfile(View view) {
+        mBinding.setSignInMode(!mBinding.getSignInMode());
     }
 }
--- app/src/main/java/com/example/youfit/SettingsFragment.java
+++ app/src/main/java/com/example/youfit/SettingsFragment.java
@@ -1,12 +1,17 @@
 package com.example.youfit;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingsFragment extends Fragment {
 
@@ -14,5 +19,21 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_settings, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+        view.findViewById(R.id.logOutBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut(); //log out
+                Intent intent = new Intent(getContext(),LoginActivity.class);
+                startActivity(intent);
+                getActivity().finish();
+            }
+        });
     }
 }
